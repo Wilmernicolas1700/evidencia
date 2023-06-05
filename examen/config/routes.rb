090df847d30cf289d5b9a8e79aee5e17b9b0be8d
Wilmer Nicolas <110918677+Wilmernicolas1700@users.noreply.github.com>
2023-06-05 17:51:57 -0500

    Examen

---
+++ examen/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'people/index'
+  get 'people/show'
+  get 'people/new'
+  get 'people/edit'
+
+  resources :people 
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
